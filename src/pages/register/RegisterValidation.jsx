30b58eb83ec36e9440b29f7e71db3658e136b33a
Natayolie <ntramier@gmail.com>
2023-05-03 09:42:55 +0200

    v1 url back prête

--- src/pages/register/RegisterValidation.jsx
+++ src/pages/register/RegisterValidation.jsx
@@ -10,7 +10,7 @@ export default function RegisterValidation (props){
                 </h2>
                 <h3>Un email de validation a été envoyé à votre adresse {props.user.email}, merci de cliquer sur le
                     lien de validation afin de finaliser l'enregistrement de votre compte.</h3>
-                <Link to="/space">
+                <Link to={`/space/${props.user.id}`}>
                     <div className="register-button">
                     <button className='register-writeButton' type='submit'>Voir mon espace</button>
                     </div>
