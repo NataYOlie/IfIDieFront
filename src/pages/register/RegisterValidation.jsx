e2005c7b3d6869f59843975ec48c33500fbf0e27
Natayolie <ntramier@gmail.com>
2023-05-14 18:16:43 +0200

    debugs acces user space funny death form modifier

--- src/pages/register/RegisterValidation.jsx
+++ src/pages/register/RegisterValidation.jsx
@@ -3,6 +3,12 @@ import {useEffect} from "react";
 
 export default function RegisterValidation (props){
 
+    function onSubmit() {
+        props.fetchUserStepTasks().then(() => {
+            window.location.href = "/space/$"${props.user.id}; // rediriger vers la page d'accueil
+        });
+    }
+
 
     return(
         <div className='register section__padding'>
@@ -12,11 +18,9 @@ export default function RegisterValidation (props){
                 </h2>
                 <h3>Un email de validation a été envoyé à votre adresse {props.user.email}, merci de cliquer sur le
                     lien de validation afin de finaliser l'enregistrement de votre compte.</h3>
-                <Link to={`/space/${props.user.id}`}>
                     <div className="register-button">
-                        <button className='register-writeButton' type='submit'>Voir mon espace</button>
+                        <button className='register-writeButton' type='submit' onSubmit={()=>onSubmit()}>Voir mon espace</button>
                     </div>
-                </Link>
             </div>
 
 
